dc0ed78cea7bce3951af9311a59bdb95b5b39bad
Dale <dalebrown5@gmail.com>
2015-01-20 09:03:33 -0700

    Prod email check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #https://db5-bloccit.herokuapp.com/
-  config.action_mailer.default_url_options = { host: 'db5-bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://db5-bloccit.herokuapp.com/' }
 
 end
