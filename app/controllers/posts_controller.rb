3999342ecf28d78719b3deb30720b1061ebbeeb3
Dale <dalebrown5@gmail.com>
2015-01-24 13:43:58 -0700

    Mocking with RSpec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
     authorize @post
 
     if @post.save
+      @post.create_vote
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post] #post show page (which is nested under topics)
     else
