d8e0d06a7c1bb7ff92e7bca797233d6f579a89b7
Dale <dalebrown5@gmail.com>
2015-01-27 15:39:25 -0700

    Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :public_view, -> { where(public: true) }
+  scope :private_view, -> { where(public: false) }
+  scope :visible_to, -> (user) { user ? all : public }
 end
